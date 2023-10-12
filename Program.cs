ec74a57d4c6c9d8744753b23d2a821a1fe1dde2e
RafyBejan <146414646+RafyBejan@users.noreply.github.com>
2023-10-12 22:54:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Impartirea2numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
